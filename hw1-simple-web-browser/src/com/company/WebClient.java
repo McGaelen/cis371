fcb531ba68f8e734b30283003ebcc494a7067583
Gaelen McIntee <gaelen.mcintee@gfs.com>
2017-08-29 17:40:58 -0400

    created socket, reader, and writer

--- hw1-simple-web-browser/src/com/company/WebClient.java
+++ hw1-simple-web-browser/src/com/company/WebClient.java
@@ -1,22 +1,53 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.*;
 
 public class WebClient {
 
     public static void main(String[] args) {
-        String hostName = "localhost";
+        final String hostName = "localhost";
+        final String cisHost = "www.cis.gvsu.edu";
+        final String demo = "/~dulimarh/demo.html";
+        final String arithemticGUI = "/~dulimarh/CS163/Examples/ArithmeticGUI.java\n";
+        final String missingFile = "/~dulimarh/MissingFile.html";
+        final String books = "/~dulimarh/books.jpg";
         int portNumber = 8000;
 
-        // Step 1: Create a socket that connects to the above host and port number
+        Socket socket;
+        PrintWriter writer;
+        BufferedReader reader;
 
-        // Step 2: Create a PrintWriter from the socket's output stream
-        //         Use the autoFlush option
+        try {
+            // Step 1: Create a socket that connects to the above host and port number
+            socket = new Socket(cisHost, portNumber);
 
-        // Step 3: Create a BufferedReader from the socket's input stream
+            // Step 2: Create a PrintWriter from the socket's output stream
+            //         Use the autoFlush option
+            writer = new PrintWriter(socket.getOutputStream(), true);
 
-        // Step 4: Send an HTTP GET request via the PrintWriter.
-        //         Remember to print the necessary blank line
+            // Step 3: Create a BufferedReader from the socket's input stream
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Step 4: Send an HTTP GET request via the PrintWriter.
+            //         Remember to print the necessary blank line
+            writer.print(
+                    "GET " + demo + " HTTP/1.1\n" +
+                    "Host: " + cisHost + "\n" +
+                    "Accept-Language: en-us\n"
+            );
+
+            System.out.println(socket);
+            System.out.println(writer);
+        } catch (IOException e) {
+            System.out.println("Couldn't connect to host.");
+            System.exit(1);
+        }
+
+
 
         // Step 5a: Read the status line of the response
         // Step 5b: Read the HTTP response via the BufferedReader until
