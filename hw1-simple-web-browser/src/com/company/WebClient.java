62f52bf2cc0a97e56ad03a83f1d76fe1ad35a800
Gaelen McIntee <gaelen.mcintee@gfs.com>
2017-08-30 21:35:07 -0400

    Prints response body to file

--- hw1-simple-web-browser/src/com/company/WebClient.java
+++ hw1-simple-web-browser/src/com/company/WebClient.java
@@ -9,7 +9,7 @@ public class WebClient {
         final String local = "localhost";
         final String cisHost = "www.cis.gvsu.edu";
         final String demo = "/~dulimarh/demo.html";
-        final String arithemticGUI = "/~dulimarh/CS163/Examples/ArithmeticGUI.java";
+        final String arithmeticGUI = "/~dulimarh/CS163/Examples/ArithmeticGUI.java";
         final String missingFile = "/~dulimarh/MissingFile.html";
         final String books = "/~dulimarh/books.jpg";
         final int localPort = 8000;
@@ -22,7 +22,7 @@ public class WebClient {
 
         try {
             // Step 1: Create a socket that connects to the above host and port number
-            socket = new Socket(local, localPort);
+            socket = new Socket(cisHost, cisPort);
 
             // Step 2: Create a PrintWriter from the socket's output stream
             //         Use the autoFlush option
@@ -33,10 +33,8 @@ public class WebClient {
 
             // Step 4: Send an HTTP GET request via the PrintWriter.
             //         Remember to print the necessary blank line
-            writer.print(
-                    "GET " + "/in.txt" + " HTTP/1.0\n" +
-                    "Host: " + local+ "\n" +
-                    "Accept-Language: en-us\n"
+            writer.println(
+                "GET " + demo + " HTTP/1.0\n"
             );
 
             // Step 5a: Read the status line of the response
@@ -44,28 +42,35 @@ public class WebClient {
 
             // Step 5b: Read the HTTP response via the BufferedReader until
             //         you get a blank line
-            String response;
-            do {
-                response = reader.readLine();
+            String response = reader.readLine();
+            while(!response.equals("")) {
                 System.out.println(response);
-            } while(!response.equals(""));
+                response = reader.readLine();
+            }
+            System.out.println();
 
             // Step 6a: Create a FileOutputStream for storing the payload
             // Step 6b: Wrap the FileOutputStream in another PrintWriter
-//            out = new PrintWriter(new FileOutputStream("./out.txt"));
+            out = new PrintWriter(new FileOutputStream("./out.txt"), false);
 
             // Step 7: Read the rest of the input from BufferedReader and write
             //         it to the second PrintWriter.
             //         Hint: readLine() returns null when there is no more data
             //         to read
+            response = reader.readLine();
+            while(response != null) {
+                System.out.println(response);
+                out.append(response + "\n");
+                response = reader.readLine();
+            }
 
             // Step 8: Remember to close the writer
-        } catch (IOException e) {
+            writer.close();
+            out.close();
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(1);
         }
-
-
-
     }
 }
