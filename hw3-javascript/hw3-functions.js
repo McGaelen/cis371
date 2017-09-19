693d3b6539d3cf6530c060da246a7a82563ac46e
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2017-09-18 20:18:04 -0400

    hw3 init commit

---
+++ hw3-javascript/hw3-functions.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Hans Dulimarta Fall 2017.
+ * TODO: Add your name below this line
+ */
+
+/**
+ * Given a node with id {rootId}, the following function finds all its descendant
+ * elements having its attribute ID set. The function returns the number of
+ * such elements. ALSO, for each such element this function sets its class
+ * to {klazName}.
+ *
+ * @param rootId
+ * @returns {number}
+ */
+function findElementsWithId(rootId, klazName) {
+    /* complete this function */
+    return 0;
+}
+
+/**
+ * The following function finds all elements with attribute 'data-gv-row' (or
+ * 'data-gv-column') and create a table of the desired dimension as a child of
+ * the element.
+ *
+ * @returns NONE
+ */
+function createTable() {
+    /* complete this function */
+}
