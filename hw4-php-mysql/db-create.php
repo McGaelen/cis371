cf87333a5f3b357415ebc2da529c4007cd613fe9
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2017-10-05 20:03:28 -0400

    started db-create.php

---
+++ hw4-php-mysql/db-create.php
@@ -0,0 +1,22 @@
+<?php
+
+$db = new mysqli('localhost', 'mcinteeg','mcinteeg','mcinteeg');
+$createFriendTable = "CREATE TABLE friend (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(30) NOT NULL,
+phone VARCHAR(30) NOT NULL,
+age INT(3)
+)";
+
+if ($db->connect_error) {
+    die('Can’t connect to db' . $db->connect_error);
+} else {
+    echo 'Connected successfully';
+}
+
+$db->query('DROP TABLE if EXISTS friend');
+
+$db->query($createFriendTable);
+
+
+?>
