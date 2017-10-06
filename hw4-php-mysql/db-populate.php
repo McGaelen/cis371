37965cd3d0f800de90c9c4e65807a9c4bcd02690
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2017-10-05 21:44:50 -0400

    restructured error if statement

--- hw4-php-mysql/db-populate.php
+++ hw4-php-mysql/db-populate.php
@@ -2,17 +2,19 @@
 $db = new mysqli('localhost', 'mcinteeg','mcinteeg','mcinteeg');
 $csvFile = $_FILES['csvFile'];
 
-if ($csvFile['error'] === 0) {
-
-    echo 'Location on server ' . $csvFile['tmp_name'];
-
-    $uploadFolder = './uploads/' . $csvFile['name'];
-
-    if (is_uploaded_file($csvFile['tmp_name'])) {
-        move_uploaded_file ($csvFile['tmp_name'], $uploadFolder);
-    }
+if ($csvFile['error'] !== 0) {
+    die('error loading csv file.');
 }
-$file = fopen('./uploads/' . $csvFile['name'], 'r');
+
+echo 'Location on server ' . $csvFile['tmp_name'];
+
+$uploadFolder = './uploads/' . $csvFile['name'];
+
+if (is_uploaded_file($csvFile['tmp_name'])) {
+    move_uploaded_file ($csvFile['tmp_name'], $uploadFolder);
+}
+
+$file = fopen($uploadFolder, 'r');
 $counter = 0;
 while ($row = fgetcsv($file)) {
     $db->query('INSERT INTO friend ( name, phone, age ) VALUES ( $row[0], $row[2], $row[3]);');
