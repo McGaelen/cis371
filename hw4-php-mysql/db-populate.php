893a1d23005faa6908643303b4656e91dd0e8527
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2017-10-05 21:23:31 -0400

    added insert query in db-populate.php

--- hw4-php-mysql/db-populate.php
+++ hw4-php-mysql/db-populate.php
@@ -1,7 +1,22 @@
 <?php
+$db = new mysqli('localhost', 'mcinteeg','mcinteeg','mcinteeg');
+$csvFile = $_FILES['csvFile'];
 
-$file = fopen( $_GET['csvFile'],'r');
+if ($csvFile['error'] === 0) {
 
-echo $file;
+    echo 'Location on server ' . $csvFile['tmp_name'];
+
+    $uploadFolder = './uploads/' . $csvFile['name'];
+
+    if (is_uploaded_file($csvFile['tmp_name'])) {
+        move_uploaded_file ($csvFile['tmp_name'], $uploadFolder);
+    }
+}
+$file = fopen('./uploads/' . $csvFile['name'], 'r');
+$counter = 0;
+while ($row = fgetcsv($file)) {
+    $db->query('INSERT INTO friend ( name, phone, age ) VALUES ( $row[0], $row[2], $row[3]);');
+    $counter++;
+}
 
 ?>
